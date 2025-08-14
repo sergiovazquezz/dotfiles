abeb3f31b0f8036f880d5fa627cd6dc33d5fbee1
sergiovazquezz <sergiovazquezle@icloud.com>
2025-08-14 02:30:36 +0200

    Simplified sketchybar config

--- sketchybar/items/volume.sh
+++ sketchybar/items/volume.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 
-volume_slider=(
-  script="$PLUGIN_DIR/volume.sh"
-  updates=on
-  label.drawing=off
-  icon.drawing=off
-  slider.highlight_color=$BLUE
-  slider.background.height=5
-  slider.background.corner_radius=3
-  slider.background.color=$BACKGROUND_2
-  slider.knob=􀀁
-  slider.knob.drawing=off
-)
-
 volume_icon=(
-  click_script="$PLUGIN_DIR/volume_click.sh"
+  script="$PLUGIN_DIR/volume.sh"
   padding_left=10
-  padding_right=0
-  icon=$VOLUME_100
-  icon.width=0
-  icon.align=left
-  icon.color=$WHITE
-  icon.font="$FONT:Regular:14.0"
-  label.width=25
+  padding_right=5
   label.align=left
+  label.drawing=off
   label.font="$FONT:Regular:14.0"
 )
 
@@ -33,15 +15,9 @@ status_bracket=(
   background.border_width=2
 )
 
-sketchybar --add slider volume right            \
-           --set volume "${volume_slider[@]}"   \
-           --subscribe volume volume_change     \
-                              mouse.clicked     \
-                              mouse.entered     \
-                              mouse.exited      \
-                                                \
-           --add item volume_icon right         \
-           --set volume_icon "${volume_icon[@]}"
+sketchybar --add item volume_icon right \
+  --set volume_icon "${volume_icon[@]}" \
+  --subscribe volume_icon volume_change
 
 sketchybar --add bracket status battery volume_icon \
-           --set status "${status_bracket[@]}"
+  --set status "${status_bracket[@]}"
--- sketchybar/plugins/volume.sh
+++ sketchybar/plugins/volume.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-WIDTH=100
-
 volume_change() {
   source "$HOME/.config/sketchybar/icons.sh"
   case $INFO in
@@ -35,42 +33,10 @@ volume_change() {
   fi
 
   sketchybar --set volume_icon icon=$ICON
-
-  sketchybar --set $NAME slider.percentage=$INFO \
-    --animate tanh 30 --set $NAME slider.width=$WIDTH
-
-  sleep 2
-
-  # Check wether the volume was changed another time while sleeping
-  FINAL_PERCENTAGE=$(sketchybar --query $NAME | jq -r ".slider.percentage")
-  if [ "$FINAL_PERCENTAGE" -eq "$INFO" ]; then
-    sketchybar --animate tanh 30 --set $NAME slider.width=0
-  fi
-}
-
-mouse_clicked() {
-  osascript -e "set volume output volume $PERCENTAGE"
-}
-
-mouse_entered() {
-  sketchybar --set $NAME slider.knob.drawing=on
-}
-
-mouse_exited() {
-  sketchybar --set $NAME slider.knob.drawing=off
 }
 
 case "$SENDER" in
 "volume_change")
   volume_change
-  ;;
-"mouse.clicked")
-  mouse_clicked
-  ;;
-"mouse.entered")
-  mouse_entered
-  ;;
-"mouse.exited")
-  mouse_exited
   ;;
 esac
